da30216deebae8f5c8a26b056d1d2b868d2b36bc
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-01-29 13:03:13 -0500

    commit tree cd51ac9de01fee06177252d0755948143faa4551

---
+++ index.js
@@ -0,0 +1,12 @@
+function myEach(collection, callback) {
+    return collection.forEach(element => callback(element));
+}
+myEach([1, 2, 3], alert);
+
+
+
+
+function myMap(collection, callback) {
+    console.log('hello');
+}
+
